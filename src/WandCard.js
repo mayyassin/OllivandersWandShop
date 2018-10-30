a3981e8bb3f009c293f95358d4c6e31c33c21287
May Yassin <mayyassin@Mays-MacBook-Pro.local>
2018-10-30 15:47:56 +0300

    task submission

---
+++ src/WandCard.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class WandCard extends Component {
+  render() {
+    return (
+      <div className="col-lg-4 col-md-6 col-12">
+        <div className="card">
+          <div className="image">
+            <img
+              className="card-img-top img-fluid"
+              src={this.props.wand.imageUrl}
+              alt={this.props.wand.core}
+            />
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">
+              <span>{this.props.wand.core}</span>
+            </h5>
+            <small className="card-text">
+              wood type: {this.props.wand.wood}
+            </small>
+            <small className="card-text">
+              wood type: {this.props.wand.length}
+            </small>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default WandCard;
