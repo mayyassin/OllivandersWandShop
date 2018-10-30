961d36ad10be02417170e54d33f59195e1f1556a
May Yassin <mayyassin@Mays-MacBook-Pro.local>
2018-10-30 16:18:55 +0300

    task submission

--- src/WandCard.js
+++ src/WandCard.js
@@ -4,10 +4,10 @@ class WandCard extends Component {
   render() {
     return (
       <div className="col-lg-4 col-md-6 col-12">
-        <div className="card">
+        <div className="card rounded-circle mb-5">
           <div className="image">
             <img
-              className="card-img-top img-fluid"
+              className="card-image card-img-top img-fluid"
               src={this.props.wand.imageUrl}
               alt={this.props.wand.core}
             />
